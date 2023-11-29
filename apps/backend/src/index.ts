8656c54b59c6b2469ce93757c8c07d08868b3e73
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-30 00:23:16 +0800

    formatting

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from "express";
-import { createServer } from "node:http";
-import { Server } from "socket.io";
+import express, { Request, Response } from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
 const { PORT } = process.env;
 const port = PORT ?? 8081;
@@ -9,12 +9,12 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
-app.get("/", (_: Request, response: Response) => {
-  return response.status(200).json({
-    message: "betterjams server healthy",
-  });
+app.get('/', (_: Request, response: Response) => {
+    return response.status(200).json({
+        message: 'betterjams server healthy'
+    });
 });
 
 server.listen(port, () => {
-  console.log(`betterjams server listening on port ${port}`);
+    console.log(`betterjams server listening on port ${port}`);
 });
