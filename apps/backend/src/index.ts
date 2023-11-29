27580e130adb616bf9bf2a29d9fb1e334124cb9c
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-11-30 00:22:45 +0800

    add server skeleton

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import express, { Request, Response } from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const { PORT } = process.env;
+const port = PORT ?? 8081;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (_: Request, response: Response) => {
+  return response.status(200).json({
+    message: "betterjams server healthy",
+  });
+});
+
+server.listen(port, () => {
+  console.log(`betterjams server listening on port ${port}`);
+});
