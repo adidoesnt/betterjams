e07cc4c095c629b63d4ff245a08bd228eae9f43b
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-12-01 18:25:39 +0800

    add icon to login page

--- apps/frontend/src/pages/login.tsx
+++ apps/frontend/src/pages/login.tsx
@@ -1,3 +1,4 @@
+import { JamIcon } from '@assets/jam';
 import Layout from '@components/layout';
 import { LoginButton } from '@components/loginButton';
 
@@ -6,6 +7,9 @@ function Login() {
         <Layout bg="bg-beige">
             <div className="flex gap-4 flex-col w-full h-full justify-center items-center">
                 <h1 className="text-4xl font-bold text-lavender">BetterJams</h1>
+                <div className='w-[30%] h-[30%]'>
+                    <JamIcon />
+                </div>
                 <LoginButton />
             </div>
         </Layout>
