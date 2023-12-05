a73319c1f0b3541c9e7a09bdc09b572d3881ecd4
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-12-06 00:28:38 +0800

    cache user playlists

--- apps/backend/src/api/services/userService.ts
+++ apps/backend/src/api/services/userService.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { cache } from 'utils/cache';
 import { v4 as uuidv4 } from 'uuid';
 import stringify from 'json-stringify-safe';
+import { token } from 'api/controllers/authController';
 
 const { CACHE_EXPIRY } = process.env;
 const exp = parseInt(CACHE_EXPIRY ?? '60');
@@ -10,12 +11,26 @@ export const cacheProfile = async (profile: any, token: string) => {
     const key = `profile-${token}`;
     const profileString = stringify(profile);
     try {
-        console.log('token set in cache');
         await cache.set(key, profileString, {
             EX: exp
         });
+        console.log('profile set in cache');
         return key;
     } catch (error: unknown) {
+        throw error;
+    }
+};
+
+export const cachePlaylists = async (playlists: any, token: string) => {
+    const key = `playlists-${token}`;
+    const playlistsString = stringify(playlists);
+    try {
+        await cache.set(key, playlistsString, {
+            EX: exp
+        });
+        console.log('playlists set in cache');
+        return key;
+    } catch (error) {
         throw error;
     }
 };
@@ -30,6 +45,22 @@ export const getProfileFromCache = async (token: string) => {
             return profile;
         }
         console.log('profile not found in cache');
+        return null;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getPlaylistsFromCache = async (token: string) => {
+    try {
+        const key = `playlists-${token}`;
+        const playlistsString = await cache.get(key);
+        if (playlistsString) {
+            const playlists = JSON.parse(playlistsString);
+            console.log('retrieved playlists from cache');
+            return playlists;
+        }
+        console.log('playlists not found in cache');
         return null;
     } catch (error) {
         throw error;
@@ -61,12 +92,15 @@ export const getUserPlaylists = async (
     playlists_endpoint: string
 ) => {
     try {
+        const cachedPlaylists = await getPlaylistsFromCache(token);
+        if (cachedPlaylists) return cachedPlaylists;
         const response = await axios.get(playlists_endpoint, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         });
         const { data: playlists } = response;
+        await cachePlaylists(playlists, token);
         return playlists;
     } catch (error: unknown) {
         throw error;
